f73178d934a0fee9d96d4356d6f7b0f961259178
Janblack07 <jandryjacob20@gmail.com>
2026-04-28 15:38:38 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ticket-rifas-app',
+  webDir: 'www'
+};
+
+export default config;
